dea2fee1d31506bfa2fe9a8ba0c0e6a9c89908cc
Raj Patel <112432655+rajpat243@users.noreply.github.com>
2024-01-26 18:17:04 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineering", "Computer Science", "Machine Learning"],
+		strings: ["Software Engineering", "Computer Science", "Machine Learning", "Web Development"],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
